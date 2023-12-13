fb6530f8fe2b1b9aab029a6f7e416186a551a1a4
戚涵睿 <12210216@mail.sustech.edu.cn>
2023-12-13 21:03:25 +0800

    Add DanmuServiceImpl class implementation

---
+++ sustc-api/src/main/java/io/sustc/service/impl/DanmuServiceImpl.java
@@ -0,0 +1,86 @@
+package io.sustc.service.impl;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import io.sustc.dto.AuthInfo;
+import io.sustc.service.DanmuService;
+
+@Service
+public class DanmuServiceImpl implements DanmuService {
+    @Autowired
+    private DataSource dataSource = new HikariDataSource();
+
+    @Override
+    public long sendDanmu(AuthInfo auth, String bv, String content, float time) {
+        try{
+            Connection conn = dataSource.getConnection();
+            if(auth == null || Authenticate.authenticate(auth, conn) == null){
+                return -1;
+            }else{
+                String sql = "INSERT INTO danmus (bv, mid, content, time, posttime) VALUES (?, ?, ?, ?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1, bv);
+                ps.setLong(2, auth.getMid());
+                ps.setString(3, content);
+                ps.setFloat(4, time);
+                ps.setTimestamp(5, new java.sql.Timestamp(System.currentTimeMillis()));
+                ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if(rs.next()){
+                    long id = rs.getLong(1);
+                    rs.close();
+                    ps.close();
+                    conn.close();
+                    return id;
+                }
+                rs.close();
+                ps.close();
+                conn.close();
+                return 0;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @Override
+    public List<Long> displayDanmu(String bv, float timeStart, float timeEnd, boolean filter) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'displayDanmu'");
+    }
+
+    @Override
+    public boolean likeDanmu(AuthInfo auth, long id) {
+        try{
+            Connection conn = dataSource.getConnection();
+            if(auth == null || Authenticate.authenticate(auth, conn) == null){
+                return false;
+            }else{
+                String sql = "INSERT INTO danmu_like (mid, danmuid) VALUES (?, ?)";
+                PreparedStatement ps = conn.prepareStatement(sql);
+                ps.setLong(1, auth.getMid());
+                ps.setLong(2, id);
+                ps.executeUpdate();
+                ps.close();
+                conn.close();
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
