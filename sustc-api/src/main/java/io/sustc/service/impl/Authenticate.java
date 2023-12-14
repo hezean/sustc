8050adbfd7485da4f812f212595cf4e80679d83a
戚涵睿 <12210216@mail.sustech.edu.cn>
2023-12-14 15:49:36 +0800

    Add video authentication and video service implementation(post and delete)

--- sustc-api/src/main/java/io/sustc/service/impl/Authenticate.java
+++ sustc-api/src/main/java/io/sustc/service/impl/Authenticate.java
@@ -4,8 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-
 import io.sustc.dto.AuthInfo;
+import io.sustc.dto.PostVideoReq;
 import io.sustc.dto.UserRecord.Identity;
 
 public class Authenticate {
@@ -38,5 +38,25 @@ public class Authenticate {
             return null;
         }
         return Identity.valueOf(rs.getString("identity").toUpperCase());
+    }
+
+    public static boolean videoAuthenticate(PostVideoReq req,AuthInfo auth, Connection conn) throws SQLException {
+        if(authenticate(auth, conn) == null||req.getTitle() == null||req.getTitle() ==""){
+            return false;
+        }else if (req.getDuration() <= 10){
+            return false;
+        }else if (req.getPublicTime().getTime() < System.currentTimeMillis()){
+            return false;
+        }else {
+            String sql = "SELECT * FROM videos WHERE ownermid = ? AND title = ?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setLong(1, auth.getMid());
+            ps.setString(2, req.getTitle());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return false;
+            }
+            return true;
+        }
     }
 }
---
+++ sustc-api/src/main/java/io/sustc/service/impl/VideoServiceImpl.java
@@ -0,0 +1,126 @@
+package io.sustc.service.impl;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+import io.sustc.dto.AuthInfo;
+import io.sustc.dto.PostVideoReq;
+import io.sustc.dto.UserRecord.Identity;
+import io.sustc.service.VideoService;
+
+@Service
+
+public class VideoServiceImpl implements VideoService {
+
+    @Autowired
+    private DataSource dataSource = new HikariDataSource();
+
+    @Override
+    public String postVideo(AuthInfo auth, PostVideoReq req) {
+        try {
+            if(Authenticate.videoAuthenticate(req, auth, dataSource.getConnection())){
+                //generate an uuid by using UUID.randomUUID().toString()
+                String bv  = UUID.randomUUID().toString();
+                String sql = "INSERT INTO videos (bv, ownermid, title, description, duration, committime, is_public) VALUES (?, ?, ?, ?, ?, ?, ?);";
+                PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
+                ps.setString(1, bv);
+                ps.setLong(2, auth.getMid());
+                ps.setString(3, req.getTitle());
+                ps.setString(4, req.getDescription());
+                ps.setFloat(5, req.getDuration());
+                ps.setTimestamp(6, req.getPublicTime());
+                ps.setBoolean(7, false);
+                ps.executeUpdate();
+                return bv;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteVideo(AuthInfo auth, String bv) {
+        try{
+            Connection conn = dataSource.getConnection();
+            Identity identity = Authenticate.authenticate(auth, conn);
+            if(identity == null ) return false;
+            String sql = "SELECT ownermid FROM videos WHERE bv = ?;";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, bv);
+            ResultSet rs = ps.executeQuery();
+            if(!rs.next()) return false;
+            if(rs.getLong("ownermid") == auth.getMid() || identity == Identity.SUPERUSER){
+                sql = "DELETE FROM videos WHERE bv = ?;";
+                ps = conn.prepareStatement(sql);
+                ps.setString(1, bv);
+                ps.executeUpdate();
+                return true;
+            } 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateVideoInfo(AuthInfo auth, String bv, PostVideoReq req) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateVideoInfo'");
+    }
+
+    @Override
+    public List<String> searchVideo(AuthInfo auth, String keywords, int pageSize, int pageNum) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'searchVideo'");
+    }
+
+    @Override
+    public double getAverageViewRate(String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAverageViewRate'");
+    }
+
+    @Override
+    public Set<Integer> getHotspot(String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHotspot'");
+    }
+
+    @Override
+    public boolean reviewVideo(AuthInfo auth, String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'reviewVideo'");
+    }
+
+    @Override
+    public boolean coinVideo(AuthInfo auth, String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'coinVideo'");
+    }
+
+    @Override
+    public boolean likeVideo(AuthInfo auth, String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'likeVideo'");
+    }
+
+    @Override
+    public boolean collectVideo(AuthInfo auth, String bv) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'collectVideo'");
+    }
+
+
+}
