f544b96a9100b9ee112db6eaf59a4064b3d3e054
hezean <realhezean@gmail.com>
2023-12-24 10:45:02 +0800

    fix: test data deserialize mismatch

--- sustc-runner/src/main/java/io/sustc/benchmark/BenchmarkService.java
+++ sustc-runner/src/main/java/io/sustc/benchmark/BenchmarkService.java
@@ -240,7 +240,7 @@ public class BenchmarkService {
 
     @BenchmarkStep(order = 9, description = "Test DanmuService#displayDanmu(String, float, float, boolean)")
     public BenchmarkResult danmuDisplay() {
-        List<Map.Entry<Object[], List<Long>>> cases = deserialize(BenchmarkConstants.TEST_DATA, BenchmarkConstants.DANMU_DISPLAY);
+        List<Map.Entry<Object[], Integer>> cases = deserialize(BenchmarkConstants.TEST_DATA, BenchmarkConstants.DANMU_DISPLAY);
         val pass = new AtomicLong();
 
         val startTime = System.currentTimeMillis();
@@ -248,10 +248,11 @@ public class BenchmarkService {
             try {
                 val args = it.getKey();
                 val res = danmuService.displayDanmu((String) args[0], (float) args[1], (float) args[2], (boolean) args[3]);
-                if (collectionEquals(it.getValue(), res)) {
+                val resSize = Objects.isNull(res) ? 0 : res.size();
+                if (it.getValue() == resSize) {
                     pass.incrementAndGet();
                 } else {
-                    log.debug("Wrong answer for {}: expected {}, got {}", it.getKey(), it.getValue(), res);
+                    log.debug("Wrong answer for {}: expected size {}, got {}", it.getKey(), it.getValue(), resSize);
                 }
             } catch (Exception e) {
                 log.error("Exception thrown for {}", it, e);
